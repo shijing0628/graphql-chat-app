91a6854bcbc99282fd6d5911f5fe2bf29f698aa5
shijing0628 <brendaliu0628@gmail.com>
2021-02-08 19:37:23 -0600

    backend work fine for fetch message data

--- README.md
+++ README.md
@@ -19,6 +19,8 @@ frontend repo link: https://github.com/shijing0628/graphql-chat-app-client
 - npm i -g sequelize-cli
 - npm install --save sequelize
 - npm install node-sass
+- sequelize db:migrate:undo:all (for seeds)
+- sequelize db:seed:all
 - sequelize model:generate --name Message --attributes content:String,uuid:uuid,from:string,to:string (cli create a message table)
   <br>
   https://www.youtube.com/watch?v=aieNcEqCLHg&list=PLMhAeHCz8S3_VYiYxpcXtMz96vePOuOX3&index=3
--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,4 +1,4 @@
-const { User } = require("../../models");
+const { User,Message } = require("../../models");
 const bcrypt = require("bcrypt");
 const { UserInputError, AuthenticationError } = require("apollo-server");
 const jwt = require("jsonwebtoken");
@@ -13,9 +13,25 @@ module.exports = {
         if (!user) throw new AuthenticationError("unauthanticated");
 
         // get user but not our own (for chat app, we only see others, ne == not equal)
-        const users = await User.findAll({
+        let users = await User.findAll({
+          attributes:['username','imageUrl','createdAt'],
           where: { username: { [Op.ne]: user.username } },
         });
+
+      const allUserMessages = await Message.findAll({
+        where:{
+          [Op.or]:[{from:user.username},{to:user.username}]
+        },
+        order:[['createdAt','DESC']]
+      })
+      
+       users= users.map(otherUser=>{
+         const latestMessage = allUserMessages.find(m=>m.from===otherUser.username || m.to===otherUser.username);
+
+         otherUser.latestMessage = latestMessage;
+
+         return otherUser;
+       })
 
         return users;
       } catch (err) {
